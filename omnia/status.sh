27324c9edce5b79dbd8e34d3f57290873b27acf0
NiklasKunkel <Niklas@theKunkels.com>
2019-07-06 19:16:08 -0700

    added more regex failsafes for message/oracle status

--- omnia/status.sh
+++ omnia/status.sh
@@ -15,9 +15,15 @@ isAssetPair () {
 
 #has interval elapsed
 isExpired () {
-	local _curTime="$1"
-	local _lastTime="$2"
-	local _expiryInterval="$3"
+	local _lastTime="$1"
+	local _expiryInterval="$2"
+	local _curTime
+	_curTime=$(timestampS)
+	if ! [[ "$_curTime" =~ ^[0-9]{10}$ ]]; then
+		error "Error - Invalid current timestamp ($_curTime)"
+		echo false
+		return 1
+	fi
 	local _expiryTime=$(( _curTime - _expiryInterval ))
 	local _expirationDif=$(( _expiryTime - _lastTime ))
 	[ "$_lastTime" -lt "$_expiryTime" ] && log "Previous price posted at t = $_lastTime is expired by $_expirationDif seconds" && echo true || echo false
@@ -28,32 +34,34 @@ isMsgExpired () {
 	local _assetPair="$1"
 	local _msg="$2"
 	local _lastTime
-	local _curTime
 	local _expirationInterval
 	_lastTime=$(echo "$_msg" | jq '.time')
-	if [[ "$_lastTime" =~ ^[0-9]{10}|[0]{1}$ ]]; then
+	if [[ "$_lastTime" =~ ^[0-9]{10}$ ]]; then
 		echo false
 		return 1
 	fi
-	_curTime=$(timestampS)
 	_expirationInterval=$(getMsgExpiration "$_assetPair")
-	[ "$(isExpired "$_curTime" "$_lastTime" "$_expirationInterval")" == "true" ] && verbose "Message timestamp is expired, skipping..." && echo true || echo false
+	[ "$(isExpired "$_lastTime" "$_expirationInterval")" == "true" ] && verbose "Message timestamp is expired, skipping..." && echo true || echo false
 }
 
 #is last price update to Oracle expired
 isOracleExpired () {
 	local _assetPair="$1"
 	local _lastTime
-	local _curTime
 	local _expirationInterval
 	_lastTime=$(pullOracleTime "$_assetPair")
-	if [[ "$_lastTime" =~ ^[0-9]{10}|[0]{1}$ ]]; then
+	if ! [[ "$_lastTime" =~ ^[0-9]{10}|[0]{1}$ ]]; then
+		error "Error - Invalid Oracle time ($_lastTime)"
 		echo false
 		return 1
 	fi
-	_curTime=$(timestampS)
 	_expirationInterval=$(getOracleExpiration "$_assetPair")
-	[ "$(isExpired "$_curTime" "$_lastTime" "$_expirationInterval")" == "true" ] && echo true || echo false
+	if ! [[ "$_expirationInterval" =~ ^[1-9][0-9]*$ ]]; then
+		error "Error - Invalid Oracle expiration interval ($_expirationInterval)"
+		echo false
+		return 1
+	fi
+	[ "$(isExpired "$_lastTime" "$_expirationInterval")" == "true" ] && echo true || echo false
 }
 
 #is spread greater than specified spread limit
@@ -62,8 +70,13 @@ isStale () {
 	local _newPrice="$2"
 	local _spreadLimit="$3"
 	local _spread
-	_spread=$(setzer spread "$_oldPrice" "$_newPrice")
 	log "Old Price = ${_oldPrice}   New Price = ${_newPrice}"
+	_spread=$(setzer spread "$_oldPrice" "$_newPrice")
+	if ! [[ "$_spread" =~ ^([1-9][0-9]*([.][0-9]+)?|[0][.][0-9]*[1-9][0-9]*|[0]{1})$ ]]; then
+		error "Error - Invalid spread ($_spread)"
+		echo false
+		return 1
+	fi
 	log "-> spread = ${_spread#-}"
 	test=$(bc <<< "${_spread#-} >= ${_spreadLimit}")
 	[[ ${test} -ne 0 ]] && log "Spread is greater than ${_spreadLimit}" && echo true || echo false
@@ -77,11 +90,17 @@ isMsgStale () {
 	local _oldPrice
 	local _spreadLimit
 	_oldPrice=$(echo "$_oldPriceMsg" | jq '.price')
-	if [[ "$_oldPrice" =~ ^([1-9][0-9]*([.][0-9]+)?|[0][.][0-9]*[1-9][0-9]*)$ ]]; then
+	if ! [[ "$_oldPrice" =~ ^([1-9][0-9]*([.][0-9]+)?|[0][.][0-9]*[1-9][0-9]*|[0]{1})$ ]]; then
+		error "Error - Invalid Message price ($_oldPrice)"
 		echo false
 		return 1
 	fi
 	_spreadLimit=$(getMsgSpread "$_assetPair")
+	if ! [[ "$_spreadLimit" =~ ^([1-9][0-9]*([.][0-9]+)?|[0][.][0-9]*[1-9][0-9]*|[0]{1})$ ]]; then
+		error "Error - Invalid Message spread limit ($_spreadLimit)"
+		echo false
+		return 1
+	fi
 	[ "$(isStale "$_oldPrice" "$_newPrice" "$_spreadLimit")" == "true" ] && echo true || echo false
 }
 
@@ -92,8 +111,14 @@ isOracleStale () {
 	local _oldPrice
 	local _spreadLimit
 	_spreadLimit=$(getOracleSpread "$_assetPair")
+	if ! [[ "$_spreadLimit" =~ ^([1-9][0-9]*([.][0-9]+)?|[0][.][0-9]*[1-9][0-9]*|[0]{1})$ ]]; then
+		error "Error - Invalid Oracle spread limit ($_spreadLimit)"
+		echo false
+		return 1
+	fi
 	_oldPrice=$(pullOraclePrice "$_assetPair")
-	if [[ "$_oldPrice" =~ ^([1-9][0-9]*([.][0-9]+)?|[0][.][0-9]*[1-9][0-9]*|[0]{1})$ ]]; then
+	if ! [[ "$_oldPrice" =~ ^([1-9][0-9]*([.][0-9]+)?|[0][.][0-9]*[1-9][0-9]*|[0]{1})$ ]]; then
+		error "Error - Invalid Oracle price ($_oldPrice)"
 		echo false
 		return 1
 	fi
@@ -108,10 +133,16 @@ isMsgNew () {
 	local _msgTime
 	_oracleTime=$(pullOracleTime "$_assetPair")
 	if ! [[ "$_oracleTime" =~ ^[0-9]{10}|[0]{1}$ ]]; then
+		error "Error - Invalid Oracle time ($_oracleTime)"
 		echo false
 		return 1
 	fi
 	_msgTime=$(echo "$_msg" | jq '.time')
+	if ! [[ "$_msgTime" =~ ^[0-9]{10}$ ]]; then
+		error "Error - Invalid Message time ($_msgTime)"
+		echo false
+		return 1
+	fi
 	[ "$_oracleTime" -gt "$_msgTime" ] && verbose "Message is older than last Oracle update, skipping..." && echo false || echo true
 }
 
@@ -122,5 +153,10 @@ isQuorum () {
 	local _quorum
 	#get min number of feeds required for quorum from Oracle contract
 	_quorum=$(pullOracleQuorum "$_assetPair")
+	if ! [[ "$_quorum" =~ ^[1-9][0-9]*$ ]]; then
+		error "Error - Invalid quorum ($_quorum)"
+		echo false
+		return 1
+	fi
 	[ "$_numFeeds" -ge "$_quorum" ] && echo true || ( echo false && verbose "Could not reach quorum ($_quorum), only $_numFeeds feeds reporting." )
 }
