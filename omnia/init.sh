a7d89b22094be08fd3e6e39341edecab7d075026
Michael Elliot <michael@digital.com.au>
2019-09-11 13:21:51 +0800

    Wait for existing ssb/omnia pid to exit before relaunching

--- omnia/init.sh
+++ omnia/init.sh
@@ -8,8 +8,10 @@ fi
 PID_SCUT=$(ps aux | grep ssb-server/bin.js | grep -v grep | awk '{print $2}')
 if ! [[ -z $PID_SCUT ]]; then
     echo "Scuttlebot is still running, killing existing instance"
-    kill "$PID_SCUT"
-    sleep 3
+    kill $PID_SCUT
+    while $(kill -0 $PID_SCUT 2>/dev/null); do
+        sleep 1
+    done
 fi
 echo "Launching Scuttlebot Server..."
 nohup "$HOME"/ssb-server/bin.js server >"$HOME"/logs/ssb-server.log 2>&1 &
@@ -24,9 +26,11 @@ PIDS_OMNIA=$(ps aux | grep omnia/omnia.sh | grep -v grep | awk '{print $2}')
 if ! [[ -z $PIDS_OMNIA ]]; then
     echo "Omnia is still running, killing existing instance"
     for PID in "${PIDS_OMNIA[@]}"; do
-	kill $PID
+        kill $PID
+        while $(kill -0 $PID 2>/dev/null); do
+            sleep 1
+        done
     done
-    sleep 3
 fi
 echo "Launching Omnia..."
 nohup "$HOME"/oracles-v2/omnia/omnia.sh >"$HOME"/logs/omnia.log 2>&1 &
