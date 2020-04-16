124dbd4e3a5d10c4025088de9cc0ff79f7c47d86
NiklasKunkel <Niklas@theKunkels.com>
2020-04-16 00:03:11 -0700

    added error check

--- omnia/config.sh
+++ omnia/config.sh
@@ -158,6 +158,7 @@ importFeeds () {
 	for feed in "${feeds[@]}"; do
 		[[ $feed =~ ^(@){1}[a-zA-Z0-9+/]{43}(=.ed25519){1}$ ]] || { error "Error - Invalid feed address: $feed"; exit 1; }
 	done
+	[[ ${errors[*]} ]] && { printf '%s\n' "${errors[@]}"; exit 1; }
 }
 
 importOptionsEnv () {
@@ -192,6 +193,8 @@ importServicesEnv () {
 	#validate cmc key
 	[[ "$CMC_API_KEY" =~ ^[0-9a-f]{8}(-){1}[0-9a-f]{4}(-){1}[0-9a-f]{4}(-){1}[0-9a-f]{4}(-){1}[0-9a-f]{12}$ ]] || errors+=("Error - Coinmarketcap API Key is invalid.")
 	export CMC_API_KEY
+
+	[[ ${errors[*]} ]] && { printf '%s\n' "${errors[@]}"; exit 1; }
 }
 
 importScuttlebotEnv () {
