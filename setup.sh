e6dae0a25de1e304ccf05df4ae0d4d607f385c14
Simon KP <simon@makerdao.com>
2020-09-25 16:06:34 +1000

    Create secrets dir

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 HOME=/home/omnia
 
+mkdir -p $HOME/secrets
 [[ -z "$OMNIA_CAPS" ]] && echo "$OMNIA_CAPS not set" || printenv OMNIA_CAPS > $HOME/secrets/caps.json
 [[ -z "$OMNIA_KEYSTORE" ]] && echo "$OMNIA_KEYSTORE not set" || printenv OMNIA_KEYSTORE > $HOME/secrets/keystore.json
 [[ -z "$OMNIA_PASSWORD" ]] && echo "$OMNIA_PASSWORD not set" || printenv OMNIA_PASSWORD > $HOME/secrets/password.txt
