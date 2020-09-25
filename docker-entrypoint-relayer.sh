3c467a388555969eff4106132336ff0ee02746fe
madumas <madumas@madumas.ca>
2020-09-25 20:12:40 +0000

    relayer in docker

---
+++ docker-entrypoint-relayer.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -o errexit
+
+HOME=/home/omnia
+
+mkdir -p $HOME/secrets
+[[ -z "$OMNIA_CAPS" ]] && echo "$OMNIA_CAPS not set" || echo -e $OMNIA_CAPS > $HOME/secrets/caps.json
+[[ -z "$OMNIA_KEYSTORE" ]] && echo "$OMNIA_KEYSTORE not set" || echo $OMNIA_KEYSTORE > $HOME/secrets/keystore.json
+[[ -z "$OMNIA_PASSWORD" ]] && echo "$OMNIA_PASSWORD not set" || echo $OMNIA_PASSWORD > $HOME/secrets/password.txt
+
+sudo -E \
+    $HOME/.nix-profile/bin/install-omnia relayer-$ETH_NET \
+        --ssb-external $EXT_IP \
+        --from $ETH_FROM \
+        --ssb-caps $HOME/secrets/caps.json \
+        --keystore $HOME/secrets \
+        --password $HOME/secrets/password.txt \
+	--network $ETH_NET \
+	--infuraKey $INFURAKEY
+
+sudo chown -R omnia $HOME/.ssb/
+cat /etc/omnia.conf
+
+/home/omnia/.nix-profile/bin/ssb-server start &
+
+sleep 10
+
+/home/omnia/.nix-profile/bin/ssb-server invite.accept $SSB_INVITE
+
+sudo -E -iu omnia omnia
