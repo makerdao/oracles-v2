7b89b2aa8bd1e32e9bcb901e6b8268cfe7b8d06a
Simon KP <simon@makerdao.com>
2020-09-26 10:06:07 +1000

    Output more stuff

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,7 @@ set -o errexit
 HOME=/home/omnia
 
 mkdir -p $HOME/secrets
+# Convert required env vars to files for install-omnia
 [[ -z "$OMNIA_CAPS" ]] && echo "OMNIA_CAPS not set" || echo -e $OMNIA_CAPS > $HOME/secrets/caps.json
 [[ -z "$OMNIA_KEYSTORE" ]] && echo "OMNIA_KEYSTORE not set" || echo $OMNIA_KEYSTORE > $HOME/secrets/keystore.json
 [[ -z "$OMNIA_PASSWORD" ]] && echo "OMNIA_PASSWORD not set" || echo $OMNIA_PASSWORD > $HOME/secrets/password.txt
@@ -19,23 +20,56 @@ sudo -E \
 
 sudo chown -R omnia $HOME/.ssb/
 
-echo ##################
+echo '##################'
+echo "MY IP"
+echo '##################'
+curl ifconfig.me
+netstat -pant
+echo '##################'
+echo "SECRETS CONFIG"
+echo '##################'
+cat $HOME/secrets/caps.json
+cat $HOME/secrets/keystore.json
+cat $HOME/secrets/password.txt
+echo '##################'
 echo "OMNIA CONFIG"
-echo ##################
+echo '##################'
 cat /etc/omnia.conf
-echo ##################
-echo "ENV VARS"
-echo ##################
-env
-echo ##################
+echo '##################'
 echo "SSB CONFIG"
-echo ##################
+echo '##################'
 cat $HOME/.ssb/config
+echo '##################'
+echo "ENV VARS"
+echo '##################'
+env
 
+echo '##################'
+echo "START SSB"
+echo '##################'
 /home/omnia/.nix-profile/bin/ssb-server start &
 
-sleep 10
+echo '##################'
+echo "SLEEP"
+echo '##################'
+sleep 20
 
+echo '##################'
+echo "SSB WHOAMI"
+echo '##################'
+ssb-server whoami
+
+echo '##################'
+echo "SSB WHOAMI - OMNIA"
+echo '##################'
+sudo -E -iu omnia ssb-server whoami
+
+echo '##################'
+echo "ACCEPT INVITE"
+echo '##################'
 /home/omnia/.nix-profile/bin/ssb-server invite.accept $SSB_INVITE
 
+echo '##################'
+echo "START OMNIA"
+echo '##################'
 sudo -E -iu omnia omnia
