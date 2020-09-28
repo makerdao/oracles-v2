f7769eda78cca8345b03a24a924819f30288956e
Simon KP <simon@makerdao.com>
2020-09-28 11:51:54 +1000

    Quote echo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,9 +6,9 @@ HOME=/home/omnia
 
 mkdir -p $HOME/secrets
 # Convert required env vars to files for install-omnia
-[[ -z "$OMNIA_CAPS" ]] && echo "OMNIA_CAPS not set" || echo -e $OMNIA_CAPS > $HOME/secrets/caps.json
-[[ -z "$OMNIA_KEYSTORE" ]] && echo "OMNIA_KEYSTORE not set" || echo $OMNIA_KEYSTORE > $HOME/secrets/keystore.json
-[[ -z "$OMNIA_PASSWORD" ]] && echo "OMNIA_PASSWORD not set" || echo $OMNIA_PASSWORD > $HOME/secrets/password.txt
+[[ -z "$OMNIA_CAPS" ]] && echo "OMNIA_CAPS not set" || echo "$OMNIA_CAPS" > $HOME/secrets/caps.json
+[[ -z "$OMNIA_KEYSTORE" ]] && echo "OMNIA_KEYSTORE not set" || echo "$OMNIA_KEYSTORE" > $HOME/secrets/keystore.json
+[[ -z "$OMNIA_PASSWORD" ]] && echo "OMNIA_PASSWORD not set" || echo "$OMNIA_PASSWORD" > $HOME/secrets/password.txt
 
 sudo -E \
     $HOME/.nix-profile/bin/install-omnia feed \
