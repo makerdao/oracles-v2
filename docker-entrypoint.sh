cd01899b89e4ad071fe1fa1229ca3ab40e5449ea
Simon KP <simon@makerdao.com>
2020-09-28 15:41:09 +1000

    nix bin

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,26 +2,27 @@
 
 # set -o errexit
 
-HOME_DIR=/home/omnia
+OMNIA_HOME=/home/omnia
+NIX_BIN=$OMNIA_HOME/.nix-profile/bin
 
-mkdir -p $HOME_DIR/secrets
+mkdir -p $OMNIA_HOME/secrets
 # Convert required env vars to files for install-omnia
-[[ -z "$OMNIA_CAPS" ]] && echo "OMNIA_CAPS not set" || echo "$OMNIA_CAPS" > $HOME_DIR/secrets/caps.json
-[[ -z "$OMNIA_KEYSTORE" ]] && echo "OMNIA_KEYSTORE not set" || echo "$OMNIA_KEYSTORE" > $HOME_DIR/secrets/keystore.json
-[[ -z "$OMNIA_PASSWORD" ]] && echo "OMNIA_PASSWORD not set" || echo "$OMNIA_PASSWORD" > $HOME_DIR/secrets/password.txt
+[[ -z "$OMNIA_CAPS" ]] && echo "OMNIA_CAPS not set" || echo "$OMNIA_CAPS" > $OMNIA_HOME/secrets/caps.json
+[[ -z "$OMNIA_KEYSTORE" ]] && echo "OMNIA_KEYSTORE not set" || echo "$OMNIA_KEYSTORE" > $OMNIA_HOME/secrets/keystore.json
+[[ -z "$OMNIA_PASSWORD" ]] && echo "OMNIA_PASSWORD not set" || echo "$OMNIA_PASSWORD" > $OMNIA_HOME/secrets/password.txt
 
 echo '##################'
 echo "INSTALL OMNIA"
 echo '##################'
 sudo -E \
-    $HOME_DIR/.nix-profile/bin/install-omnia feed \
+    $NIX_BIN/install-omnia feed \
         --ssb-external $EXT_IP \
         --from $ETH_FROM \
-        --ssb-caps $HOME_DIR/secrets/caps.json \
-        --keystore $HOME_DIR/secrets \
-        --password $HOME_DIR/secrets/password.txt
+        --ssb-caps $OMNIA_HOME/secrets/caps.json \
+        --keystore $OMNIA_HOME/secrets \
+        --password $OMNIA_HOME/secrets/password.txt
 
-sudo chown -R omnia $HOME_DIR/.ssb/
+sudo chown -R omnia $OMNIA_HOME/.ssb/
 
 echo '##################'
 echo "OMNIA CONFIG"
@@ -31,16 +32,16 @@ cat /etc/omnia.conf
 echo '##################'
 echo "START SSB"
 echo '##################'
-/home/omnia/.nix-profile/bin/ssb-server start &
+$NIX_BIN/ssb-server start &
 
 sleep 40
 
 echo '##################'
 echo "ACCEPT INVITE"
 echo '##################'
-/home/omnia/.nix-profile/bin/ssb-server invite.accept $SSB_INVITE
+$NIX_BIN/ssb-server invite.accept $SSB_INVITE
 
 echo '##################'
 echo "START OMNIA"
 echo '##################'
-sudo -u omnia omnia
+sudo -u omnia $NIX_BIN/omnia
