990233e996efd5d41d8e90c941f6e38e15253250
Simon KP <simon@makerdao.com>
2020-09-28 12:04:24 +1000

    Start Omnia

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,6 +10,9 @@ mkdir -p $HOME/secrets
 [[ -z "$OMNIA_KEYSTORE" ]] && echo "OMNIA_KEYSTORE not set" || echo "$OMNIA_KEYSTORE" > $HOME/secrets/keystore.json
 [[ -z "$OMNIA_PASSWORD" ]] && echo "OMNIA_PASSWORD not set" || echo "$OMNIA_PASSWORD" > $HOME/secrets/password.txt
 
+echo '##################'
+echo "INSTALL OMNIA"
+echo '##################'
 sudo -E \
     $HOME/.nix-profile/bin/install-omnia feed \
         --ssb-external $EXT_IP \
@@ -21,42 +24,21 @@ sudo -E \
 sudo chown -R omnia $HOME/.ssb/
 
 echo '##################'
-echo "MY IP"
-echo '##################'
-curl ifconfig.me
-netstat -pant
-echo '##################'
-echo "SECRETS CONFIG"
-echo '##################'
-cat $HOME/secrets/caps.json
-cat $HOME/secrets/keystore.json
-cat $HOME/secrets/password.txt
-echo '##################'
 echo "OMNIA CONFIG"
 echo '##################'
 cat /etc/omnia.conf
-echo '##################'
-echo "SSB CONFIG"
-echo '##################'
-cat $HOME/.ssb/config
-echo '##################'
-echo "ENV VARS"
-echo '##################'
-env
-
-sleep infinity
 
 echo '##################'
 echo "START SSB"
 echo '##################'
-# /home/omnia/.nix-profile/bin/ssb-server start &
+/home/omnia/.nix-profile/bin/ssb-server start &
 
-#echo '##################'
-#echo "ACCEPT INVITE"
-#echo '##################'
-#/home/omnia/.nix-profile/bin/ssb-server invite.accept $SSB_INVITE
-#
-#echo '##################'
-#echo "START OMNIA"
-#echo '##################'
-#sudo -E -iu omnia omnia
+echo '##################'
+echo "ACCEPT INVITE"
+echo '##################'
+/home/omnia/.nix-profile/bin/ssb-server invite.accept $SSB_INVITE
+
+echo '##################'
+echo "START OMNIA"
+echo '##################'
+sudo -E -iu omnia omnia
