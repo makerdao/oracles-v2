2e53807a44182eedd99d258274d88373893f02f7
Simon KP <simon@makerdao.com>
2020-09-28 14:00:33 +1000

    Sleep longer

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,26 +2,26 @@
 
 set -o errexit
 
-HOME=/home/omnia
+HOME_DIR=/home/omnia
 
-mkdir -p $HOME/secrets
+mkdir -p $HOME_DIR/secrets
 # Convert required env vars to files for install-omnia
-[[ -z "$OMNIA_CAPS" ]] && echo "OMNIA_CAPS not set" || echo "$OMNIA_CAPS" > $HOME/secrets/caps.json
-[[ -z "$OMNIA_KEYSTORE" ]] && echo "OMNIA_KEYSTORE not set" || echo "$OMNIA_KEYSTORE" > $HOME/secrets/keystore.json
-[[ -z "$OMNIA_PASSWORD" ]] && echo "OMNIA_PASSWORD not set" || echo "$OMNIA_PASSWORD" > $HOME/secrets/password.txt
+[[ -z "$OMNIA_CAPS" ]] && echo "OMNIA_CAPS not set" || echo "$OMNIA_CAPS" > $HOME_DIR/secrets/caps.json
+[[ -z "$OMNIA_KEYSTORE" ]] && echo "OMNIA_KEYSTORE not set" || echo "$OMNIA_KEYSTORE" > $HOME_DIR/secrets/keystore.json
+[[ -z "$OMNIA_PASSWORD" ]] && echo "OMNIA_PASSWORD not set" || echo "$OMNIA_PASSWORD" > $HOME_DIR/secrets/password.txt
 
 echo '##################'
 echo "INSTALL OMNIA"
 echo '##################'
 sudo -E \
-    $HOME/.nix-profile/bin/install-omnia feed \
+    $HOME_DIR/.nix-profile/bin/install-omnia feed \
         --ssb-external $EXT_IP \
         --from $ETH_FROM \
-        --ssb-caps $HOME/secrets/caps.json \
-        --keystore $HOME/secrets \
-        --password $HOME/secrets/password.txt
+        --ssb-caps $HOME_DIR/secrets/caps.json \
+        --keystore $HOME_DIR/secrets \
+        --password $HOME_DIR/secrets/password.txt
 
-sudo chown -R omnia $HOME/.ssb/
+sudo chown -R omnia $HOME_DIR/.ssb/
 
 echo '##################'
 echo "OMNIA CONFIG"
@@ -43,5 +43,4 @@ echo '##################'
 echo '##################'
 echo "START OMNIA"
 echo '##################'
-sudo -E -iu omnia env
-sudo -E -iu omnia omnia &
+sudo -E -iu omnia omnia
