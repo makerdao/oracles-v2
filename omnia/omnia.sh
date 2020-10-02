3e5e05fba89602150487339c6be38f0528d19e51
Paweł Zaremba <pawzar@gmail.com>
2020-10-02 17:47:47 +0200

    New function to support gofer queries

--- omnia/omnia.sh
+++ omnia/omnia.sh
@@ -165,7 +165,6 @@ execute () {
 
 		#broadcast message to scuttelbot
 		broadcastPriceMsg "$assetPair" "$median" "$medianHex" "$time" "$timeHex" "$hash" "$sig" "${validSources[@]}" "${validPrices[@]}"
-	
 	done
 }
 
