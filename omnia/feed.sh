44d94182260e6c19ac0cf79abe03aeb2a811a70d
Paweł Zaremba <pawzar@gmail.com>
2020-10-05 14:08:26 +0200

    A small reorg to facilitate testing

---
+++ omnia/feed.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/env bash
+
+#publish new price messages for all assets
+broadcastAllPriceMessages () {
+	for assetPair in "${assetPairs[@]}"; do
+		validSources=()
+		validPrices=()
+		median=0
+
+		log "Querying ${assetPair^^} prices and calculating median..."
+		readSources "$assetPair"
+
+		if [[ "${#validPrices[@]}" -lt 2 ]] || [[ "${#validSources[@]}" -lt 2 ]] || [[ "${#validPrices[@]}" -ne "${#validSources[@]}" ]]; then
+			error "Error - Failed to fetch sufficient valid prices from sources."
+			continue
+		fi
+
+		if [ "$(isPriceValid "$median")" == "false" ]; then
+			error "Error - Failed to calculate valid median: ($median)"
+			debug "Sources = ${validSources[*]}"
+			debug "Prices = ${validPrices[*]}"
+			continue
+		fi
+
+		#Get latest message for asset pair
+		latestMsg=$(pullLatestFeedMsgOfType "$SCUTTLEBOT_FEED_ID" "$assetPair")
+
+		if [ "$(isEmpty "$latestMsg")" == "false" ] \
+		&& [ "$(isAssetPair "$assetPair" "$latestMsg")" == "true" ] \
+		&& [ "$(isMsgExpired "$assetPair" "$latestMsg")" == "false" ] \
+		&& [ "$(isMsgStale "$assetPair" "$latestMsg" "$median")" == "false" ]
+		then
+			continue
+		fi
+
+		#Get timestamp
+		time=$(timestampS)
+		if [[ ! "$time" =~ ^[1-9]{1}[0-9]{9}$ ]]; then
+			error "Error - Got invalid timestamp"
+			debug "Invalid Timestamp = $time"
+			continue
+		fi
+
+		#Convert timestamp to hex
+		timeHex=$(time2Hex "$time")
+		if [[ ! "$timeHex" =~ ^[0-9a-fA-F]{64}$ ]]; then
+			error "Error - Failed to convert timestamp to hex"
+			debug "Timestamp = $time"
+			debug "Invalid Timestamp Hex = $timeHex"
+			continue
+		fi
+
+		#Convert median to hex
+		medianHex=$(price2Hex "$median")
+		if [[ ! "$medianHex" =~ ^[0-9a-fA-F]{64}$ ]]; then
+			error "Error - Failed to convert median to hex:"
+			debug "Median = $median"
+			debug "Invalid Median Hex = $medianHex"
+			continue
+		fi
+
+		#Convert asset pair to hex
+		assetPairHex=$(seth --to-bytes32 "$(seth --from-ascii "$assetPair")")
+		if [[ ! "$assetPairHex" =~ ^[0-9a-fA-F]{64}$ ]]; then
+			error "Error - Failed to convert asset pair to hex:"
+			debug "Asset Pair = $assetPair"
+			debug "Invalid Asset Pair Hex = $assetPairHex"
+			continue
+		fi
+
+		#Create hash
+		hash=$(keccak256Hash "0x" "$medianHex" "$timeHex" "$assetPairHex")
+		if [[ ! "$hash" =~ ^(0x){1}[0-9a-fA-F]{64}$ ]]; then
+			error "Error - failed to generate valid hash"
+			debug "Median Hex = $medianHex"
+			debug "Timestamp Hex = $timeHex"
+			debug "Asset Pair Hex = $assetPairHex"
+			debug "Invalid Hash = $hash"
+			continue
+		fi
+
+		#Sign hash
+		sig=$(signMessage "$hash")
+		if [[ ! "$sig" =~ ^(0x){1}[0-9a-f]{130}$ ]]; then
+			error "Error - Failed to generate valid signature"
+			debug "Hash = $hash"
+			debug "Invalid Signature = $sig"
+			continue
+		fi
+
+		#broadcast message to scuttelbot
+		broadcastPriceMsg "$assetPair" "$median" "$medianHex" "$time" "$timeHex" "$hash" "$sig" "${validSources[@]}" "${validPrices[@]}"
+	done
+}
