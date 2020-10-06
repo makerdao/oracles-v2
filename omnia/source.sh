e56eb0bb00c0499e243e9afd451afdd894e85dc7
Paweł Zaremba <pawzar@gmail.com>
2020-10-06 16:11:08 +0200

    Loop over all configured source tools

--- omnia/source.sh
+++ omnia/source.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 
-mapSetzer() {
-	local _assetPair=$1
-	local _source=$2
-	echo "$_source" "$(setzer price "$_assetPair" "$_source")"
-}
-export -f mapSetzer
-
-#read price data of asset
-readSourcesWithSetzer () {
+readSourcesWithSetzer()  {
 	local _assetPair="${1,,}"
 	local _prices
 	local _price
@@ -27,26 +19,21 @@ readSourcesWithSetzer () {
 	for i in "${!_prices[@]}"; do
 		_source=${_prices[$i]% *}
 		_price=${_prices[$i]#* }
-		addPriceFromSource "$_source" "$_price"
+		_addPriceFromSource "$_source" "$_price"
 	done
 
 	median=$(getMedian "${validPrices[@]}")
 	verbose "median => $median"
 }
 
-addPriceFromSource () {
-	local _source="$1"
-	local _price="$(LC_ALL=POSIX printf "%f" "$2")"
-	if [[ "$(isPriceValid "$_price")" == "true" ]]; then
-		validSources+=( "$_source" )
-		validPrices+=( "$_price" )
-		verbose "$_source => $_price"
-	else
-		error "Error - [$_source] Invalid price data = $_price"
-	fi
+_mapSetzer() {
+	local _assetPair=$1
+	local _source=$2
+	echo "$_source" "$(setzer price "$_assetPair" "$_source")"
 }
+export -f _mapSetzer
 
-readSourcesWithGofer ()  {
+readSourcesWithGofer()   {
 	local  _output
 	_output=$(gofer price --format json "${1}")
 	local _jqFilter='
@@ -59,27 +46,26 @@ readSourcesWithGofer ()  {
 	'
 
 	local _prices
-	mapfile -t _prices < <(echo "${_output}" | jq -r "$_jqFilter" 2>/dev/null)
+	mapfile -t _prices < <(echo "${_output}" | jq -r "$_jqFilter" 2> /dev/null)
 
 	for i in "${!_prices[@]}"; do
 		_source=${_prices[$i]% *}
 		_price=${_prices[$i]#* }
-		addPriceFromSource "$_source" "$_price"
+		_addPriceFromSource "$_source" "$_price"
 	done
 
 	median=$(echo "${_output}" | jq -r '.[0].price')
-  verbose "median => $median"
+	verbose  "median => $median"
 }
 
-readSources() {
-	local _assetPair="$1"
-
-	if [[ "$OMNIA_FEED_SOURCE" == "setzer" ]]; then
-		readSourcesWithSetzer "$_assetPair"
-	elif [[ "$OMNIA_FEED_SOURCE" == "gofer" ]]; then
-		readSourcesWithGofer "$_assetPair"
+_addPriceFromSource()  {
+	local _source="$1"
+	local _price="$(LC_ALL=POSIX printf "%f" "$2")"
+	if [[ "$(isPriceValid "$_price")" == "true" ]]; then
+		validSources+=("$_source")
+		validPrices+=("$_price")
+		verbose "$_source => $_price"
 	else
-		error "Error - Unknown Feed Source: $OMNIA_FEED_SOURCE"
-		return
+		error "Error - [$_source] Invalid price data = $_price"
 	fi
 }
