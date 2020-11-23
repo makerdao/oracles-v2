43ed7fa7a6258f4e32e3b511675de1457c6e565c
Christopher Fredén <me@icetan.org>
2020-11-23 15:08:14 +0100

    Add gofer regression test [ch6571]

--- nix/docker.nix
+++ nix/docker.nix
@@ -2,7 +2,9 @@ let
   srcs = import ../nix/srcs.nix;
 
   inherit (builtins) readFile;
-  inherit (srcs) pkgs ssb-server omnia;
+  inherit (srcs) pkgs ssb-server;
+
+  omnia = srcs.omnia { gofer = import ../gofer {}; };
 
   path = with pkgs; lib.makeBinPath [
     coreutils bash jq gnused
