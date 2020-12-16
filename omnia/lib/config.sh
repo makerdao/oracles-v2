074b5a46bd66aa9ee6b2bf1c49ee9a0f04b52a89
Christopher Fredén <me@icetan.org>
2020-12-16 14:43:10 +0100

    Fix bug in gofer config options

--- omnia/lib/config.sh
+++ omnia/lib/config.sh
@@ -223,7 +223,7 @@ importOptionsEnv () {
 		[[ "$SETZER_MIN_MEDIAN" =~ ^[1-9][0-9]*$ ]] || errors+=("Error - Setzer Minimum Median param is invalid, must be positive integer.")
 		export SETZER_MIN_MEDIAN
 
-		if jq -e '.sources | index("gofer") != null' <<<"$_json"; then
+		if jq -e '.sources | index("gofer") != null' <<<"$_config"; then
 			OMNIA_GOFER_CONFIG="${OMNIA_GOFER_CONFIG:-$(echo "$_json" | jq -r '.goferConfig // "./gofer.json"')}"
 			[[ -e "$OMNIA_GOFER_CONFIG" ]] || errors+=("Error - Gofer config file '$OMNIA_GOFER_CONFIG' doesn't exist.")
 			export OMNIA_GOFER_CONFIG
