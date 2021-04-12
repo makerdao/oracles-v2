cee2283b5f60f4a304134754897eb4de1ae14449
Paweł Zaremba <pawzar@gmail.com>
2021-04-12 14:15:54 +0200

    Update release script

--- shell.nix
+++ shell.nix
@@ -22,71 +22,93 @@ pkgs.mkShell rec {
     updateNodePackages() {
       (cd "$ROOT_DIR"/nix && {
          node2nix -i node-packages.json -c nodepkgs.nix --nodejs-10
-      } )
+      })
     }
 
     version() {
-      cat $VERSION_FILE
+      cat "$VERSION_FILE"
     }
 
     release() {
-      local branch=$(git rev-parse --abbrev-ref HEAD)
-      local oldVersion=$(cat $VERSION_FILE)
-      if [[ $1 =~ -?-?help ]]; then
+      local _level="$1"
+      if [[ -z "$_level" || $_level =~ -?-?help ]]; then
         echo >&2 "Usage: release SEMVER"
         semver --help
-      elif [[ $1 =~ minor|major ]]; then
+        return 1
+      fi
+
+      if [[ ! $_level =~ ^pre && $2 != "--force" && $2 != "-f" ]]; then
+        _level="pre$_level"
+      fi
+
+      local branch
+      local oldVersion
+      local version
+
+      branch=$(git rev-parse --abbrev-ref HEAD)
+      oldVersion=$(cat "$VERSION_FILE")
+
+      if [[ $_level =~ minor|major$ ]]; then
         [[ $branch == master ]] || {
           echo >&2 "Not on master branch, checkout 'master' to make a new release branch."
           return 1
         }
-        local version=$(semver -i "$1" $oldVersion)
-        echo $version > "$VERSION_FILE"
 
-        git commit -m "Bump >$1< version to $version" "$VERSION_FILE"
-        git tag v$version-rc && {
+        version=$(semver -i "$_level" --preid rc "$oldVersion")
+
+        local _branchVersion
+        _branchVersion=$(semver -i "$version")
+        _branchVersion=''${_branchVersion%.*}
+
+        echo "$version" > "$VERSION_FILE"
+        git commit -m "Start '$_branchVersion' release line" "$VERSION_FILE"
+        git tag "v$version" && {
           echo >&2 "To publish this commit as a release candidate run:"
-          echo "   git push --atomic origin master master:release/''${version%.0} v$version-rc"
+          echo "   git push --atomic origin master master:release/$_branchVersion v$version"
           echo >&2
-          echo >&2 "To patch this $1 release checkout the release branch:"
-          echo "   git checkout release/''${version%.0}"
+          echo >&2 "To patch this '$_level' release checkout the release branch:"
+          echo "   git checkout release/$_branchVersion"
         }
-      else
+      elif [[ $_level =~ patch|prerelease$ ]]; then
         [[ $branch =~ ^release/ ]] || {
-          echo >&2 "Not on a release branch, checkout a 'release/*' or create one: release minor or release major"
+          echo >&2 "Not on a release branch, checkout a 'release/*' or create one by: release minor|major"
           return 1
         }
-        if [[ -n $1 ]]; then
-          local version=$(semver -i "$1" --preid rc $oldVersion)
-          echo $version > "$VERSION_FILE"
 
-          git commit -m "Bump \`$1\` version to $version" "$VERSION_FILE"
-          git tag v$version && {
-            echo >&2 "To publish this commit as a release candidate run:"
-            echo "   git push --atomic origin $branch v$version"
-          }
-        else
-          [[ $oldVersion =~ -rc\. ]] || {
-            echo >&2 "Current version ($oldVersion) not a Release Candidate. Run: release premajor|preminor|prepatch|prerelease"
-            return 1
-          }
-          local version=$(semver -i $oldVersion)
-          echo $version > "$VERSION_FILE"
+        version=$(semver -i "$_level" --preid rc "$oldVersion")
 
-          git commit -m "Release \`$version\` as \`stable\`" "$VERSION_FILE"
-          git tag v$version && {
-            git tag -f stable
-            echo >&2 "To publish this commit as a stable release run:"
-            echo "   git push -f origin stable"
-          }
-        fi
+        echo "$version" > "$VERSION_FILE"
+        git commit -m "Bump '$_level' version to $version" "$VERSION_FILE"
+        git tag "v$version" && {
+          echo >&2 "To publish this commit as a release candidate run:"
+          echo "   git push --atomic origin $branch v$version"
+        }
+      elif [[ $_level == "stable" ]]; then
+        [[ $oldVersion =~ -rc\. ]] || {
+          echo >&2 "Current version ($oldVersion) is not a Release Candidate. Run: release major|minor|patch"
+          return 1
+        }
+
+        version=$(semver -i "$oldVersion")
+
+        echo "$version" > "$VERSION_FILE"
+        git commit -m "Release 'v$version' as 'stable'" "$VERSION_FILE"
+        git tag "v$version" && {
+          git tag -f stable
+          echo >&2 "To publish this commit as a stable release run:"
+          echo "   git push -f origin stable"
+        }
+      else
+        echo >&2 "Unknown release level ($_level)"
+        return 1
       fi
     }
+
     echo 'Locally available commands:
       * updateNodePackages
       * version
          see current release version
       * release [--help] [LEVEL]
-         - LEVEL: major, minor, patch, premajor, preminor, prepatch, or prerelease'
+        - LEVEL: major, minor, patch or prerelease'
   '';
 }
