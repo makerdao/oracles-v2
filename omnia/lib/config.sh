4e0fb93d7d1c9a3ffa82fa98361ea5c0e98fb866
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2021-08-04 18:56:28 +0300

    Fixed gas price config parsing

--- omnia/lib/config.sh
+++ omnia/lib/config.sh
@@ -81,14 +81,14 @@ importGasPrice () {
 	local _json="$1"
 
 	# Getting Gas price details
-	ETH_GAS_SOURCE="$(echo "$_json" | jq -rS '.ethereum.gasPrice.source // "node"')"
+	ETH_GAS_SOURCE="$(echo "$_json" | jq -rS '.gasPrice.source // "node"')"
 	export ETH_GAS_SOURCE
 
-	ETH_GAS_MULTIPLIER="$(echo "$_json" | jq -r '.ethereum.gasPrice.multiplier // 1')"
+	ETH_GAS_MULTIPLIER="$(echo "$_json" | jq -r '.gasPrice.multiplier // 1')"
 	[[ $ETH_GAS_MULTIPLIER =~ ^[0-9\.]+$ ]] || errors+=("Error - Ethereum Gas price multiplier is invalid, should be a number.")
 	export ETH_GAS_MULTIPLIER
 	
-	ETH_GAS_PRIORITY="$(echo "$_json" | jq -r '.ethereum.gasPrice.priority // "fast"')"
+	ETH_GAS_PRIORITY="$(echo "$_json" | jq -r '.gasPrice.priority // "fast"')"
 	[[ $ETH_GAS_PRIORITY =~ ^(slow|standard|fast|fastest)$ ]] || errors+=("Error - Ethereum Gas price priority is invalid.\nValid options are: slow, standard, fast, fastest.")
 	export ETH_GAS_PRIORITY
 
