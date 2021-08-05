652b71d08d3b45e4ea070859c1882cd9db1b5649
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2021-08-05 15:34:08 +0300

    Added config gas price exact values tests

--- omnia/test/config.sh
+++ omnia/test/config.sh
@@ -4,34 +4,40 @@ root_path=$(cd "$test_path/.."; pwd)
 lib_path="$root_path/lib"
 
 . "$lib_path/log.sh"
+. "$lib_path/util.sh"
 . "$lib_path/config.sh"
 
 . "$root_path/tap.sh" 2>/dev/null || . "$root_path/../tests/lib/tap.sh"
+
+_validConfig="$(jq -c . "$test_path/configs/oracle-relayer-test.conf")"
 
 # Setting up clean vars
 ETH_GAS_SOURCE=""
 ETH_GAS_MULTIPLIER=""
 ETH_GAS_PRIORITY=""
 
-_validConfig="$(jq -c . "$test_path/configs/oracle-relayer-test.conf")"
-
-_json="{\"gasPrice\":{\"source\":\"node\",\"multiplier\":\"test\"}}"
-assert "importGasPrice should fail in case of invalid multiplier" fail importGasPrice $_json
-
-_json="{\"gasPrice\":{\"source\":\"node\",\"multiplier\":1,\"priority\":\"invalid\"}}"
-assert "importGasPrice should fail in case of invalid priority" fail importGasPrice $_json
-
-# Happy path
-ETH_GAS_SOURCE=""
-ETH_GAS_MULTIPLIER=""
-ETH_GAS_PRIORITY=""
-
-_json=$(jq -S '.ethereum' < "$_validConfig")
+_json=$(jq -c '.ethereum' <<< "$_validConfig")
 assert "importGasPrice should correctly parse values" run importGasPrice $_json
 
 assert "ETH_GAS_SOURCE should have value: node" match "node" <<<$ETH_GAS_SOURCE
 assert "ETH_GAS_MULTIPLIER should have value: 1" match "1" <<<$ETH_GAS_MULTIPLIER
 assert "ETH_GAS_PRIORITY should have value: fast" match "fast" <<<$ETH_GAS_PRIORITY
+
+# Helper function
+exact () {
+  [[ $1 == $2 ]] || { cat <<EOF
+got: $1
+expect: $2
+EOF
+return 1
+  }
+}
+export -f exact
+
+# Testing exact values, no quotes/anything else
+assert "ETH_GAS_SOURCE should be exact value: node" run exact $ETH_GAS_SOURCE 'node'
+assert "ETH_GAS_MULTIPLIER should be exact value: 1" run exact $ETH_GAS_MULTIPLIER '1'
+assert "ETH_GAS_PRIORITY should be exact value: fast" run exact $ETH_GAS_PRIORITY 'fast'
 
 # Testing changed values
 _json="{\"gasPrice\":{\"source\":\"gasnow\",\"multiplier\":2,\"priority\":\"slow\"}}"
