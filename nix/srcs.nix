de1923562a7436d22c6153090c354db350f10370
Paweł Zaremba <pawzar@gmail.com>
2021-10-11 14:37:35 +0200

    Remove `hevm` dependency

--- nix/srcs.nix
+++ nix/srcs.nix
@@ -1,13 +1,26 @@
 let
   inherit (builtins) map filter listToAttrs attrValues isString currentSystem;
-  inherit (import sources.nixpkgs { }) pkgs;
+  inherit (import sources.nixpkgs {
+    overlays = [
+      (import "${sources.dapptools}/overlay.nix")
+      (self: super: {
+        hevm = null;
+        solc = null;
+      })
+      (self: super: {
+        seth = self.callPackage (import "${sources.dapptools}/src/seth") { };
+        ethsign = self.callPackage (import "${sources.dapptools}/src/ethsign") { };
+      })
+    ];
+  })
+    pkgs;
   inherit (pkgs.lib.strings) removePrefix;
 
   getName = x: let parse = drv: (builtins.parseDrvName drv).name; in if isString x then parse x else x.pname or (parse x.name);
 
   sources = import ./sources.nix;
   ssb-patches = ../ssb-server;
-  dapptools = import sources.dapptools { overlays = [ (self: super: { hevm = true; }) ]; };
+
 in rec {
   inherit pkgs;
 
@@ -33,10 +46,7 @@ in rec {
 
   stark-cli = pkgs.callPackage ../starkware { };
 
-  omnia = pkgs.callPackage ../omnia {
-    inherit ssb-server setzer-mcd stark-cli oracle-suite;
-    inherit (dapptools) seth ethsign;
-  };
+  omnia = pkgs.callPackage ../omnia { inherit ssb-server setzer-mcd stark-cli oracle-suite; };
 
   install-omnia = pkgs.callPackage ../systemd { inherit omnia ssb-server oracle-suite; };
 }
